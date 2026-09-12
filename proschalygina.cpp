acbd36eb0d7ad2bdce85f365293b6634b5e19235
Olga Proschalygina <olga.pi53@mail.ru>
2026-09-12 18:05:24 +0700

    Добавлены файлу к варианту 68

---
+++ proschalygina.cpp
@@ -0,0 +1,11 @@
+#include "proschalygina.h"
+
+// »з литров в галлоны
+double lToGal(double l) {
+    return l / 3.78541;
+}
+
+// »з галлонов в литры
+double galToL(double gal) {
+    return gal * 3.78541;
+}
